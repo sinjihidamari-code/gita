87582a2cd5b2a9e821e5d6c3dc5150d399d55881
sinjihidamari-code <sinji.hidamari@gmail.com>
2026-02-07 14:57:01 +0900

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "3298288430243834e40bd869cbf0bee8
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "be6ff72176611e6bf1cdaee04a407d22",
+"flutter_bootstrap.js": "73c673654b4449fd965dfbf1f94a6112",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
