bc32cce4b32da6f51c5472cda09d7eb9818815f7
sinjihidamari-code <sinji.hidamari@gmail.com>
2026-02-07 10:22:53 +0900

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2118045345"
+    serviceWorkerVersion: "2856003458"
   }
 });
